94fcc28fd9f0cfa929a6c5bbc78853bbc47f53f7
baranovsky-s <baranovsky.sergii@gmail.com>
2019-09-19 11:32:57 +0300

    Updates

---
+++ precache-manifest.5d2ccd03476cd5ba26677df1577e4ce8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a22d5230937483259cd38c84e70364",
+    "url": "/react.js-movie-db/index.html"
+  },
+  {
+    "revision": "0822013d78b5418ead38",
+    "url": "/react.js-movie-db/static/css/main.f6a725d1.chunk.css"
+  },
+  {
+    "revision": "e0114475f93a84a1bf88",
+    "url": "/react.js-movie-db/static/js/2.54de005d.chunk.js"
+  },
+  {
+    "revision": "0822013d78b5418ead38",
+    "url": "/react.js-movie-db/static/js/main.f7153c59.chunk.js"
+  },
+  {
+    "revision": "31fdb239337483404f8a",
+    "url": "/react.js-movie-db/static/js/runtime~main.922c65c3.js"
+  }
+]);
